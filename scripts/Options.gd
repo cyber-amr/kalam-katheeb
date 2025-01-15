37d2a8667bce00ea73df569f76dd3192f0a457a0
Amr <amr@programmer.net>
2025-01-16 01:30:53 +0300

    fix options saving/loading issues

--- scripts/Options.gd
+++ scripts/Options.gd
@@ -5,6 +5,10 @@ const SAVEFILE_PATH := "user://.options.json"
 var data := get_default_options()
 
 
+func _ready() -> void:
+	load_locally()
+
+
 func get_default_options() -> Dictionary:
 	return {}
 
@@ -12,7 +16,7 @@ func get_default_options() -> Dictionary:
 func save_locally() -> Error:
 	var file := FileAccess.open(SAVEFILE_PATH, FileAccess.WRITE)
 	if !file:
-		return file.get_open_error()
+		return FileAccess.get_open_error()
 	
 	file.store_string(JSON.stringify(data, "  "))
 	
@@ -23,7 +27,8 @@ func save_locally() -> Error:
 func load_locally() -> Error:
 	var file := FileAccess.open(SAVEFILE_PATH, FileAccess.READ)
 	if !file:
-		return file.get_open_error()
+		push_warning("Could not load options data: [%s]" % [FileAccess.get_open_error()])
+		return FileAccess.get_open_error()
 	
 	var json = JSON.parse_string(file.get_as_text())
 	file.close()
@@ -31,7 +36,7 @@ func load_locally() -> Error:
 	if json is Dictionary:
 		data = json
 	else:
-		push_error("Failed to parse save file as Dictionary")
+		push_error("Failed to parse options data as Dictionary")
 		return ERR_PARSE_ERROR
 	
 	return OK
